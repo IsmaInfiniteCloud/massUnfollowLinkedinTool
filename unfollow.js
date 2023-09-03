6240776ff6e1b2c4021f6710f74e0d66b2f34d7d
IsmaInfiniteCloud <manou141@hotmail.com>
2023-09-03 12:00:39 -0400

    Update unfollow.js

--- unfollow.js
+++ unfollow.js
@@ -1,30 +1,29 @@
 console.log('Script started');
 
-// Function to scroll down
+// Function to scroll down the page
 function scrollDown() {
-    console.log(`Scrolled to: ${window.scrollY}`);
-    window.scrollTo(0, window.scrollY + 500);
+    let previousHeight = window.scrollY;
+    window.scrollTo(0, previousHeight + 500);
+    console.log('Scrolled to:', window.scrollY);
 }
 
-// Function to click the unfollow button and then confirm the unfollow action
+// Function to click "Unfollow" and confirm it
 function clickUnfollowAndConfirm() {
     console.log('Trying to find unfollow buttons...');
 
-    let unfollowButtons = document.querySelectorAll('[aria-label="Click to stop following"]');
-    if (unfollowButtons.length > 0) {
-        console.log('Found unfollow button, clicking...');
-        unfollowButtons[0].click();
-        
-        setTimeout(() => {
-            console.log('Looking for name element in popup...');
-            let nameElement = document.querySelector('[data-test-dialog-content]');
-            if (nameElement) {
-                console.log(`Name element found: ${nameElement.textContent}`);
-            } else {
-                console.log('Name element not found in popup.');
-            }
+    let unfollowButtonSelector = '.unfollow-button-class'; // Replace with the actual class or ID
+    let unfollowButtons = document.querySelectorAll(unfollowButtonSelector);
 
+    if (unfollowButtons.length > 0) {
+        console.log(`Found ${unfollowButtons.length} unfollow buttons, clicking the first one...`);
+
+        // Click the first "Unfollow" button
+        unfollowButtons[0].click();
+
+        // Wait for the popup to appear
+        setTimeout(() => {
             console.log('Looking for confirm button in popup...');
+
             let confirmButton = document.querySelector('[data-test-dialog-primary-btn]');
             if (confirmButton) {
                 console.log('Confirm button found, clicking...');
